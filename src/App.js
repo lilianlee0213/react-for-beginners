5a0bd7129f622b0dbdfcb3a5941cb4b9972ae9e2
Lilian Lee <lilianlee0213@gmail.com>
2023-04-01 20:49:19 -0400

    cleanup function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import Button from './Button';
+import {useEffect, useState} from 'react';
+// import Button from './Button';
+
+function Hello() {
+	function byeFn() {
+		console.log('bye :(');
+	}
+	function hiFn() {
+		console.log('hi :)');
+		return byeFn;
+	}
+
+	useEffect(hiFn, []);
+	return <h1>Hello</h1>;
+}
 function App() {
+	const [showing, setShowing] = useState(false);
+	const onClick = () => {
+		setShowing((prev) => !prev);
+	};
 	return (
 		<div>
-			<h1>Welcome back!</h1>
-			<Button text={'Click Me!'} />
+			{showing ? <Hello /> : null}
+			<button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
 		</div>
 	);
 }
