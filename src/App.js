1b56db7e17eeaf0811303890f91d0457dde63864
Lilian Lee <lilianlee0213@gmail.com>
2023-04-03 14:36:57 -0400

    To To List

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import {useEffect, useState} from 'react';
 // import Button from './Button';
 
-function Hello() {
-	function byeFn() {
-		console.log('bye :(');
-	}
-	function hiFn() {
-		console.log('hi :)');
-		return byeFn;
-	}
-
-	useEffect(hiFn, []);
-	return <h1>Hello</h1>;
-}
 function App() {
-	const [showing, setShowing] = useState(false);
-	const onClick = () => {
-		setShowing((prev) => !prev);
+	const [toDo, setToDo] = useState('');
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === '') {
+			return;
+		}
+		setToDos((currentArray) => [toDo, ...currentArray]);
+		setToDo('');
 	};
 	return (
 		<div>
-			{showing ? <Hello /> : null}
-			<button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+			<h1>My Todos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={toDo}
+					type="text"
+					placeholder="Write your to do..."
+				/>
+				<button>Add To Do</button>
+			</form>
+			<hr />
+			{toDos.map((item, index) => {
+				return <li key={index}>{item}</li>;
+			})}
 		</div>
 	);
 }
